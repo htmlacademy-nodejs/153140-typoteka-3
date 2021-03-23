be17d2e8cdb54145a8ae78930ff336057910dd0e
Anastasiya Vasina <nast791@mail.ru>
2021-03-23 21:12:15 +0300

    module3-task2

--- src/express/express.js
+++ src/express/express.js
@@ -4,9 +4,14 @@ const express = require(`express`);
 const mainRoutes = require(`./routes/main`);
 const myRoutes = require(`./routes/my`);
 const articlesRoutes = require(`./routes/articles`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 const app = express();
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
